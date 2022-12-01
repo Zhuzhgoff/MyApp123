21e5ea7631c841a069bd626df5a6e5ea8f385bf0
Zhuzhgoff <vobla.03@mail.ru>
2022-12-01 16:27:10 +0700

    Completed

--- app/src/main/java/com/example/myapplication/ContactActivity.kt
+++ app/src/main/java/com/example/myapplication/ContactActivity.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import dialogYesOrNo
 
 class ContactActivity: AppCompatActivity()  {
@@ -28,13 +31,32 @@ class ContactActivity: AppCompatActivity()  {
         TextName.text = "Имя:      " + objects?.name
         TextPhone.text = "Телефон:   " + objects?.telephone
 
-        TextPhone.setOnClickListener {
+        /*TextPhone.setOnClickListener {
             val dialIntent = Intent(Intent.ACTION_DIAL)
             dialIntent.data = Uri.parse("tel:"+objects?.telephone)
             startActivity(dialIntent)
+        }*/
+        TextPhone.setOnClickListener {
+            val REQUEST_CODE = 1
 
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.CALL_PHONE),
+                    REQUEST_CODE
+                )
+
+            } else {
+                val callIntent = Intent(Intent.ACTION_CALL)
+                callIntent.data = Uri.parse("tel:" + objects?.telephone)
+                startActivity(callIntent)
+
+            }
         }
-
         buttonChange.setOnClickListener {
             val intent = Intent(this@ContactActivity, ContactChangeActivity::class.java)
             intent.putExtra("Id", id)
